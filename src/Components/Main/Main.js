a6bf60a6e4cee64d237b725ce275c18b6eba7895
brenley-woodard <brenleyew@gmail.com>
2023-01-19 14:40:30 -0800

    added usePokemon function

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { usePokemon } from '../../hooks/pokehook';
 import { fetchPokemon } from '../../services/pokefetch';
 
 export default function Main() {
   return <main>
-    { fetchPokemon() }
   </main>;
 }
---
+++ src/hooks/pokehook.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { fetchPokemon } from '../services/pokefetch';
+
+export function usePokemon() {
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+        // load our pokemon
+      const resp = await fetchPokemon();
+        // set out pokemon state
+      setPokemon(resp);
+    };
+    fetchData();
+  }, []);
+
+  return pokemon;
+} 
