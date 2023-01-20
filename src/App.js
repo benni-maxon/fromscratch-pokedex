0f861130fec146914b40489846ea354a038e4d5d
benni maxon <benni.maxon@gmail.com>
2023-01-20 13:17:11 -0800

    created placeholder header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
 import './App.css';
+import Header from './Components/Display/Header/Header.js';
 import Main from './Components/Main/Main.js';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
-
     </div>
   );
 }
