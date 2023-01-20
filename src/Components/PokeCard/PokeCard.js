cc1100a9790c6cf5ae59573f62e12fa7450e898c
benni maxon <benni.maxon@gmail.com>
2023-01-20 12:00:10 -0800

    NA types now don't display

--- src/Components/PokeCard/PokeCard.js
+++ src/Components/PokeCard/PokeCard.js
@@ -5,11 +5,14 @@ export default function PokeCard({ pokemon, species_id, type_1, type_2, url_imag
     <div>
       <img src={url_image} />
       <h2>{pokemon}</h2>
-      <h3>{species_id}</h3>
-      <h3>
-        {/* maybe later prevent null displays for single type pokemon */}
-        {type_1} & {type_2}
-      </h3>
+      <h3>#{species_id}</h3>
+      {type_2 === 'NA' ? (
+        <h3>{type_1}</h3>
+      ) : (
+        <h3>
+          {type_1} & {type_2}
+        </h3>
+      )}
     </div>
   );
 }
