5fa6fb766b23e3a0582a1dacc2c454ac9b7a382f
benni maxon <benni.maxon@gmail.com>
2023-01-20 13:13:46 -0800

    some styling

--- src/Components/PokeCard/PokeCard.js
+++ src/Components/PokeCard/PokeCard.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import './PokeCard.css';
 
 export default function PokeCard({ pokemon, species_id, type_1, type_2, url_image }) {
   return (
-    <div>
-      <img src={url_image} />
+    <div className="card">
       <h2>{pokemon}</h2>
-      <h3>#{species_id}</h3>
+      <img src={url_image} />
       {type_2 === 'NA' ? (
-        <h3>{type_1}</h3>
+        <h3 className="types">Type: {type_1}</h3>
       ) : (
-        <h3>
-          {type_1} & {type_2}
+        <h3 className="types">
+          Types: {type_1} & {type_2}
         </h3>
       )}
+      <h3 className="species-id">#{species_id}</h3>
     </div>
   );
 }
